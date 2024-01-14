b3986eb170d56357e95e23d4e4f0b8177aea2522
sysmat <tomaz.majerhold@gmail.com>
2024-01-14 09:54:41 +0100

    reservations

---
+++ my-app/src/routes/reservation.tsx
@@ -0,0 +1,10 @@
+import { Title } from "@solidjs/meta";
+
+export default function Home() {
+    return (
+      <main>
+        <Title>reservations</Title>
+        <h1>reservations</h1>
+      </main>
+    );
+  }
